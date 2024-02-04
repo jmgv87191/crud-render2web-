24602474cee50371aa76c1462f3abc42f1312839
jmgv87191 <jmgv87191@gmail.com>
2024-02-04 15:37:59 -0700

    borrado

--- conexion.php
+++ conexion.php
@@ -1,9 +1,5 @@
 <?php
 
-$servername = "162.241.203.240";
-$user = "juangtzc_jmgv8719";
-$password = "Jmsa424b";
-$database = "juangtzc_portafolio";
 
 $conn = new mysqli($servername,$user,$password,$database);
 
