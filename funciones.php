9d2410ccaa2b551bed529f13bdd1b9939b792ff0
jmgv87191 <jmgv87191@gmail.com>
2024-02-04 16:03:46 -0700

    carperta img

--- funciones.php
+++ funciones.php
@@ -2,7 +2,9 @@
 
     function subir_imagen(){
         if (isset($_FILES["imagen_usuario"])) {
-            
+            $extension = explode('.', $_FILES['imagen_usuario']['name']);
+            $nuevo_nombre = rand(). '.' . $extension[1];
+            $ubicacion = './img/'.$nuevo_nombre
         }
     }
 
