4f6e3919b0578403b8c44418d2dfa61c63b851b9
jmgv87191 <jmgv87191@gmail.com>
2024-02-04 17:24:39 -0700

    video 10

--- funciones.php
+++ funciones.php
@@ -4,8 +4,27 @@
         if (isset($_FILES["imagen_usuario"])) {
             $extension = explode('.', $_FILES['imagen_usuario']['name']);
             $nuevo_nombre = rand(). '.' . $extension[1];
-            $ubicacion = './img/'.$nuevo_nombre
+            $ubicacion = './img/'.$nuevo_nombre;
+            move_uploaded_file($_FILES['imagen_usuario']['tmp_name'],$ubicacion);
         }
+    }
+
+    function obtener_nombre_imagen('$id_usuario'){
+        include('conexion.php');
+        $stmt = $conexion->prepare("SELECT imagen FROM usuarios WHERE id= '$id_usuario' ");
+        $stmt-> execute();
+        $resultado = $stmt->fetchAll();
+        foreach($resultado as $fila){
+            return $fila['imagen'];
+        }
+    }
+
+    function obtener_todos_registros(){
+        include('conexion.php');
+        $stmt = $conexion->prepare("SELECT * FROM usuarios");
+        $stmt-> execute();
+        $resultado = $stmt->fetchAll();
+        return $stmt->rowCount();
     }
 
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-new DataTable('#example');
+$(document).ready(function(){
+    
+    // new DataTable('#example');
+
+    var dataTable = $('#datos_usuario').DataTable({
+        "processing":true,
+        "serverSide": true,
+        "order":[],
+        "ajax":{
+            url:"../obtener_registro.php",
+            type: "POST",
+        },
+        "columnsDefs":[
+            {
+                "targets": [0,3,4],
+                "orderable":false
+            }
+        ]
+    })
+})
+
+
